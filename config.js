eec1b9eee2072e4152a635f4a158fe44de299332
Yubin QIU <Yubin.Qiu@kp.org>
2018-11-17 13:57:04 -0800

    yubin:update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  REST_ENDPOINT: JSON.stringify('http://68.183.160.80/wp-json/wp/v2'),
+  REST_ENDPOINT: JSON.stringify('http://loveplanet.live/wp-json/wp/v2'),
   POSTS_PER_PAGE: JSON.stringify(9),
   GA_TRACKING_ID: JSON.stringify(isProd ? 'UA-111438908-1' : 'XXX'),
   REQUEST_CACHE_MAX: JSON.stringify(150)
