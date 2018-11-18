2b6c7dff2921289edcab4882beb537413f50066f
Yubin QIU <Yubin.Qiu@kp.org>
2018-11-18 01:04:17 -0800

    yubin:update foot

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,8 +1,51 @@
 <template>
-  <footer>
-    Made by Susan&Quinn
+<footer id="fh5co-footer" role="contentinfo">
+  <div class="container">
+    <div class="row row-pb-md">
+      <div class="col-md-4 fh5co-widget">
+        <h4>LovePlanet</h4>
+        <p>True Life and Make Fun</p>
+      </div>
+      <div class="col-md-4 col-md-push-1">
+        <h4>Links</h4>
+        <ul class="fh5co-footer-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Lifestyle</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
 
-  </footer>
+      <div class="col-md-4 col-md-push-1">
+        <h4>Contact Information</h4>
+        <ul class="fh5co-footer-links">
+          <li><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
+          <li><a href="http://freehtml5.co">FreeHTML5.co</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div class="row copyright">
+      <div class="col-md-12 text-center">
+        <p>
+          <small class="block">&copy; 2016 LovePlanet. All Rights Reserved.</small>
+          <small class="block">Designed by Susan Lin & Yubin Qiu</small>
+        </p>
+        <p>
+          <ul class="fh5co-social-icons">
+            <li><a href="#"><i class="icon-twitter"></i></a></li>
+            <li><a href="#"><i class="icon-facebook"></i></a></li>
+            <li><a href="#"><i class="icon-linkedin"></i></a></li>
+            <li><a href="#"><i class="icon-dribbble"></i></a></li>
+          </ul>
+        </p>
+      </div>
+    </div>
+
+  </div>
+</footer>
 </template>
 
 <script>
@@ -12,13 +55,5 @@
 </script>
 
 <style scoped lang="scss">
-  footer {
-    text-align: center;
-    padding: 1rem;
-    background: $gray--light;
-  }
 
-  a {
-    font-weight: bold;
-  }
 </style>
