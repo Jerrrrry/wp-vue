9d953b3153d530b54c35d4b0c9fa1ff33f9d38bc
Yubin QIU <Yubin.Qiu@kp.org>
2018-11-19 01:52:28 -0800

    yubin:update the home posts

--- src/components/Homeposts.vue
+++ src/components/Homeposts.vue
@@ -42,11 +42,74 @@
 </template>
 
 <script>
+
+import bus from '../bus';
+import ajax from '../mixins/ajax';
+
 export default {
   name: 'Homeposts',
 
+  mixins: [ajax],
+
   data () {
-    return {}
+    return {
+      posts: [],
+      page: 1,
+      totalPages: null
+    }
+  },
+
+  mounted: function () {
+    this.getPosts();
+  },
+
+  methods: {
+    getPosts: async function () {
+      let response;
+
+      try {
+        response = await this.get(
+          `/posts?per_page=${POSTS_PER_PAGE}&page=${this.page}`
+        );
+        this.totalPages = response.headers['x-wp-totalpages'];
+      } catch (error) {
+
+        return;
+      }
+
+      this.posts = await this.getFeaturedImages(response.data);
+    },
+
+
+
+    getFeaturedImages: function (posts) {
+      return new Promise((resolve) => {
+        let requests = posts.map((post) => {
+          return new Promise( async (resolve) => {
+            let response;
+
+            try {
+
+              if(post.featured_media <= 0) {
+                console.log('no pic');
+                throw "No featured image.";
+              }
+
+              response = await this.get(
+                `/media/${post.featured_media}`
+              );
+              post.featured_image = response.data.media_details.sizes['medium'].source_url;
+            } catch (error) {
+              post.featured_image = null;
+            }
+
+            resolve(post);
+          });
+        });
+
+        Promise.all(requests).then((posts) => resolve(posts));
+      });
+    }
   },
 
 }
