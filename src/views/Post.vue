bd6217b9008be3a67e0895fca9dfc414b1d251ec
Yubin QIU <Yubin.Qiu@kp.org>
2018-11-19 23:48:04 -0800

    yubin:updaet the

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -29,16 +29,9 @@
                     <div class="single-blog-content">
                         <div class="line"></div>
                         <a href="#" class="post-tag">Lifestyle</a>
-                        <h4><a href="#" class="post-headline mb-0">Party people in the house</a></h4>
-                        <div class="post-meta mb-50">
-                            <p>By <a href="#">james smith</a></p>
-                            <p>3 comments</p>
-                        </div>
-                        <p>Curabitur venenatis efficitur lorem sed tempor. Integer aliquet tempor cursus. Nullam vestibulum convallis risus vel condimentum. Nullam auctor lorem in libero luctus, vel volutpat quam tincidunt. Nullam vestibulum convallis risus vel condimentum. Nullam auctor lorem in libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestibulum mauris quis aliquam. Integer accumsan sodales odio, id tempus velit ullamcorper id. Quisque at erat eu libero consequat tempus. Quisque molestie convallis tempus. Ut semper purus metus, a euismod sapien sodales ac. Duis viverra eleifend fermentum. Donec sagittis lacus sit amet augue sodales, vel cursus enim tristique. Maecenas vitae massa ut est consectetur sagittis quis vitae tortor. Sed et massa vel nisi dapibus pretium et et leo.</p>
+                        <h4><a href="#" class="post-headline mb-0">{{title}}</a></h4>
 
-                        <p>Curabitur venenatis efficitur lorem sed tempor. Integer aliquet tempor cursus. Nullam vestibulum convallis risus vel condimentum. Nullam auctor lorem in libero luctus, vel volutpat quam tincidunt. Morbi sodales, dolor id ultricies dictum, diam odio tempor purus, at ultrices elit nulla ac nisl. Vestibulum enim sapien, blandit finibus elit vitae, venenatis tempor enim. Ut varius eros at fringilla aliquet. Sed sed sodales quam. Nam fermentum sed turpis sollicitudin lobortis.</p>
-
-                        <p>Proin a nibh dignissim, volutpat mauris vitae, pellentesque nisi. In euismod non ligula at gravida. Nunc blandit eget enim vel mattis. Sed semper, purus ac suscipit scelerisque, eros dui fermentum tortor, vitae facilisis lacus nulla sit amet diam. Nullam eget sagittis mi. Suspendisse vitae volutpat lorem. Cras porta velit id sagittis ultrices. Maecenas efficitur tellus ac aliquam molestie. Morbi vel ipsum gravida, ultricies nunc sed, posuere purus. Donec ipsum lectus, congue ut fermentum vitae, molestie hendrerit erat. Sed lacinia accumsan egestas. Cras ac ipsum a ante placerat pellentesque.</p>
+                        <PostBody :content="content"></PostBody>
                     </div>
                 </div>
 
@@ -132,7 +125,7 @@
 
           response = await this.get(`/media/${id}`);
         } catch (error) {
-          return null;
+          return '/img/bg-img/b5.jpg';
         }
 
         return response.data.media_details.sizes['medium'].source_url;
@@ -146,58 +139,5 @@
 </script>
 
 <style scoped lang="scss">
-  article {
-    max-width: 900px;
-    margin: 0 auto;
-    background: $gray--light;
-    border: 2px solid darken($gray--light, 5%);
-    padding: 1rem;
 
-    @include media($small) {
-      padding: 3rem;
-    }
-  }
-
-  header {
-    margin-bottom: 1rem;
-  }
-
-  h1 {
-    margin: 2rem 0 0;
-  }
-
-  ul {
-    @include media($mobile) {
-      display: grid;
-      grid-template-columns: auto 1fr;
-      grid-gap: 5px;
-    }
-  }
-
-  li {
-
-    & + & {
-      &:before {
-        @include media($mobile) {
-          content: '|';
-          float: left;
-          margin: 0 5px 0 0;
-        }
-      }
-    }
-
-    a {
-      color: inherit;
-      font-weight: 600;
-
-      &:hover {
-        color: $action-color;
-      }
-    }
-  }
-
-  a {
-    display: block;
-    margin-bottom: 1rem;
-  }
 </style>
