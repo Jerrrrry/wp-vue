ce617f9516bb41468f23777003a2090892b2bf2f
Yubin QIU <Yubin.Qiu@kp.org>
2018-11-26 14:24:01 -0800

    yubin:update logo link

--- src/components/Topheader.vue
+++ src/components/Topheader.vue
@@ -33,7 +33,7 @@
       <div class="container h-100">
           <div class="row h-100 align-items-center">
               <div class="col-12">
-                  <a href="index.html" class="original-logo"><img src="/img/core-img/logo.png" alt=""></a>
+                  <a href="/index" class="original-logo"><img src="/img/core-img/logo.png" alt=""></a>
               </div>
           </div>
       </div>
