eb80a4a79edb639705a240889745baac03d0a41f
Yubin QIU <Yubin.Qiu@kp.org>
2018-11-26 14:17:18 -0800

    yubin:update

--- src/components/Topheader.vue
+++ src/components/Topheader.vue
@@ -21,7 +21,7 @@
               <!-- Top Social Area -->
               <div class="col-12 col-sm-4">
                   <div class="top-social-area">
-                      <a href="www.linkedin.com/in/yubin-qiu-1101" data-toggle="tooltip" data-placement="bottom" title="Linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                      <a href="https://www.linkedin.com/in/yubin-qiu-1101/" target="_blank" data-toggle="tooltip" data-placement="bottom" title="Linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                   </div>
               </div>
           </div>
